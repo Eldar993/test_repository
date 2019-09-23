51d41bb45b462521c28bd35b9e75eec193be2701
Eldar <eldar7989@gmail.com>
2019-09-23 22:51:14 +0200

    World to Pretty World

--- src/com/test/Foo.java
+++ src/com/test/Foo.java
@@ -2,7 +2,7 @@ package com.test;
 
 public class Foo {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Pretty World!");
     }
 }
 
