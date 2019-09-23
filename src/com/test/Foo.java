03d23bc130c4e74a6bc5d6c226097254d974d8dc
Eldar <eldar7989@gmail.com>
2019-09-23 22:53:16 +0200

    Add test method

--- src/com/test/Foo.java
+++ src/com/test/Foo.java
@@ -4,5 +4,9 @@ public class Foo {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    private static void test() {
+        System.out.println("I'm a test");
+    }
 }
 
